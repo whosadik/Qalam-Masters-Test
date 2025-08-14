f2651b0ba11f49b740518f4cd414723825097ff1
whosadik <adikmanmedet@gmail.com>
2025-08-15 00:33:22 +0500

    refactoring

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-  base: "/Qalam-Masters-2.0/",
+  base: "/Qalam-Masters-Test/",
   plugins: [react()],
   resolve: {
     alias: {
