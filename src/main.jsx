b916f0bae905c42d771039f46af212fb19080ef9
whosadik <adikmanmedet@gmail.com>
2025-08-15 17:20:03 +0500

    Navbar fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter basename="/Qalam-Masters-Test">
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
