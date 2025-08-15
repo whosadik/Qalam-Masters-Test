787a3a16320fef5347f97a805220d1159832b8af
whosadik <adikmanmedet@gmail.com>
2025-08-15 17:28:15 +0500

    router changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter basename="/Qalam-Masters-Test">
-        <App />
-      </BrowserRouter>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
