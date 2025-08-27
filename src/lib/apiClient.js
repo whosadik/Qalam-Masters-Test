1b372811c103b279745319e877df075a3cd7955e
whosadik <adikmanmedet@gmail.com>
2025-08-27 13:03:44 +0500

    fix apiClient

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 20000,
 });
 
